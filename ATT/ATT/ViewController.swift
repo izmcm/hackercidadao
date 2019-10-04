eb8226080d3f801b3a9adf1fc0b08fcb8048a947
vinicius emanuel <vncemanuel@gmail.com>
2019-10-04 16:38:12 -0300

    take photo

--- ATT/ATT/ViewController.swift
+++ ATT/ATT/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let vcCamera = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.vcCamera.delegate = self
+        self.vcCamera.allowsEditing = true
+        self.vcCamera.sourceType = .camera
     }
 
-
+    @IBAction func btnTakePictureTapped(_ sender: Any) {
+        self.opeCamera()
+    }
+    
+    func opeCamera(){
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        switch status {
+        case .authorized:
+            self.present(self.vcCamera, animated: true, completion: nil)
+        case .denied:
+            print("negada")
+        default:
+            AVCaptureDevice.requestAccess(for: .video, completionHandler: { granted in
+                if granted {
+                    self.present(self.vcCamera, animated: true, completion: nil)
+                } else {
+                    print("Denied access ")
+                }
+            })
+            return
+        }
+    }
+    
 }
 
+extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.vcCamera.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+        
+        print(image.size)
+    }
+}
