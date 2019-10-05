6774cf7569dc8dfd752d267af4a0f02862cc2ff7
vinicius emanuel <vncemanuel@gmail.com>
2019-10-05 10:15:49 -0300

    base send comment

--- ATT/ATT/controllers/ChatViewController.swift
+++ ATT/ATT/controllers/ChatViewController.swift
@@ -15,6 +15,7 @@ class ChatViewController: UIViewController {
     var location: CLLocation?
     @IBOutlet weak var tableView: UITableView!
     var locationSended = false
+    @IBOutlet weak var tableViewBottomConstrant: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +24,6 @@ class ChatViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        guard let location = self.location else{ return }
-        
         guard let img = self.img else {return}
         
         FirebaseHelper.share.sendImage(image: img, block: {
@@ -34,17 +33,13 @@ class ChatViewController: UIViewController {
             self.tableView.reloadData()
         })
         
+        self.configKeyboard()
     }
     
     func fetchLocationFrom(location: CLLocation){
         CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
             if error == nil{
                 let mark = placemarks?.first
-                
-                print("cidade:",mark?.locality)
-                print("bairro:",mark?.subLocality)
-                print("rua:",mark?.thoroughfare)
-                print("número:",mark?.subThoroughfare)
                 
                 FirebaseHelper.share.sendAdress(cidade: mark?.locality ?? "", bairro: mark?.subLocality ?? "", rua: mark?.thoroughfare ?? "", numero: mark?.subThoroughfare ?? "", lat: self.location?.coordinate.latitude ?? 0, lon: self.location?.coordinate.longitude ?? 0, block: {
                     let newMessage = Message(type: .Server, text: "Quantas vítimas o acidente possui?")
@@ -106,12 +101,53 @@ extension ChatViewController: UITableViewDataSource{
             case .Comment:
                 let cell = tableView.dequeueReusableCell(withIdentifier: "commentCell") as! CommentTableViewCell
                 cell.delegate = self
+                cell.txtComment.resignFirstResponder()
                 return cell
             }
         }
         
         return UITableViewCell()
     }
+    
+        func configKeyboard() {
+    
+            NotificationCenter.default.addObserver(self,
+                                                   selector: #selector(keyboardDidAppear),
+                                                   name: UIResponder.keyboardWillShowNotification,
+                                                   object: nil)
+    
+            NotificationCenter.default.addObserver(self,
+                                                   selector: #selector(keyboardWillHide),
+                                                   name: UIResponder.keyboardWillHideNotification,
+                                                   object: nil)
+    
+            let tapToHideKeyboard = UITapGestureRecognizer(target: self,
+                                                           action: #selector(hideKeyboard))
+            tapToHideKeyboard.numberOfTapsRequired = 1
+            self.view?.addGestureRecognizer(tapToHideKeyboard)
+        }
+    
+        @objc func hideKeyboard() {
+            self.view.endEditing(true)
+        }
+    
+        @objc func keyboardDidAppear(notification: Notification) {
+    
+            let screenSize = UIScreen.main.bounds.size
+            let keyboardFrame:NSValue = (notification.userInfo! as NSDictionary).value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            
+            self.tableViewBottomConstrant.constant = keyboardHeight + 20
+            self.view.layoutIfNeeded()
+        }
+    
+        @objc func keyboardWillHide() {
+            
+            self.tableViewBottomConstrant.constant = 0
+            
+            self.view.layoutIfNeeded()
+
+        }
 }
 
 extension ChatViewController: BooleanTableCellDelegate, SelectionCellDelegate, SliderCellDelegate, CommentTableViewCellDelegate {
